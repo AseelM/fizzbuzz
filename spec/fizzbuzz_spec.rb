0b057d6a1c373c3021b4cf4e88304ba2c0979455
aseelm <aseelmustafa@outlook.com>
2018-03-18 02:43:11 +0000

    writes fifth test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -16,4 +16,8 @@ describe 'fizzbuzz' do
   it 'returns 2 when passed 2' do
     expect(fizzbuzz(2)).to eq 2
   end
+
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
 end
